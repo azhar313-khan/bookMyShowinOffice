35eb2ef32f6ce5cd983d17b39abf8ca13c0e91df
azhar <azhar.khan@thoughtwin.com>
2025-03-17 12:11:34 +0530

    add website on render

--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -12,8 +12,12 @@ const swaggerDefinition = {
   },
   servers: [
     {
-      url: "http://localhost:3000",
-      description: "Development server",
+      url: "https://bookmyshowinoffice.onrender.com", // Your Render URL
+      description: "Production Server",
+    },
+    {
+      url: "http://localhost:3000", // Local development server
+      description: "Local Development",
     },
   ],
 };
@@ -27,5 +31,5 @@ const swaggerSpec = swaggerJSDoc(options);
 
 export const setupSwagger = (app: Express) => {
   app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-  console.log("Swagger Docs available at http://localhost:3000/api-docs");
+  console.log("Swagger docs available at /api-docs");
 };
